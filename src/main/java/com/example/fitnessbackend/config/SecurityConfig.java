be0589bd0468d264637df72acf256be665fa9aab
Ben <bmaduabuchi02@gmail,com>
2025-05-15 13:29:07 -0500

    added health check screening

--- src/main/java/com/example/fitnessbackend/config/SecurityConfig.java
+++ src/main/java/com/example/fitnessbackend/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig{
                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                .authorizeHttpRequests(auth -> auth
                        .requestMatchers("/api/auth/**").permitAll()
+                       .requestMatchers("/actuator/health", "/actuator/health/**").permitAll()
                        .anyRequest().authenticated()
                )
                .exceptionHandling(ex -> ex
